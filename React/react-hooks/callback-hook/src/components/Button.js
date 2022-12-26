1932fb76a920fa393194a3284a5245320fa02db6
Onkar <onkar4414@gmail.com>
2022-12-26 23:27:13 +0530

    initial commit

---
+++ React/react-hooks/callback-hook/src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Button({ handleClick, children }) {
+  console.log('Rendering button - ', children)
+  return (
+    <button onClick={handleClick}>
+      {children}
+    </button>
+  )
+}
+
+export default React.memo(Button)
+//export default Button
---
+++ React/react-hooks/callback-hook/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title() {
+  console.log('Rendering Title')
+  return (
+    <h2>
+      useCallback Hook
+    </h2>
+  )
+}
+
+export default React.memo(Title)
+//export default Title;
